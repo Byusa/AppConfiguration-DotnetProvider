52851bcae918821a0a72309391bb94a60da87b29
Avani Gupta <avanigupta@users.noreply.github.com>
2020-10-16 14:41:13 -0700

    Example code to avoid Key Vault reference exception

---
+++ examples/ConsoleApplication/CustomSecretClient.cs
@@ -0,0 +1,39 @@
+﻿using Azure;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class CustomSecretClient : SecretClient
+    {
+        class MockResponse<T> : Response<T>
+        {
+            private T _value;
+            public override T Value => _value;
+
+            public MockResponse(T value)
+            {
+                _value = value;
+            }
+
+            public override Response GetRawResponse()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override Uri VaultUri { get; }
+            
+        public CustomSecretClient(Uri vaultUri)
+        {
+            VaultUri = vaultUri ?? new Uri("https://test.vault.azure.net");
+        }
+
+        public override Task<Response<KeyVaultSecret>> GetSecretAsync(string name, string version = null, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult((Response<KeyVaultSecret>)new MockResponse<KeyVaultSecret>(null));
+        }
+    }
+}
